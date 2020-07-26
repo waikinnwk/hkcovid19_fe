f10ff780ac776252ed0b94ce743e0f66d32044cd
waikinnwk <waikinnwk@gmail.com>
2020-07-26 20:05:27 +0800

    commit - today data

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask import g
 from flask_socketio import SocketIO
-
+from flask_babel import Babel
 
 app = Flask(__name__, template_folder="templates")
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'locales'
 socketio = SocketIO(app, cors_allowed_origins='*')
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return getattr(g, 'LOCALE', 'en')
 
 from app import views
