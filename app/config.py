1240f3f39505431e8d748acec0d453b94239681b
waikinnwk <waikinnwk@gmail.com>
2020-08-02 21:08:07 +0800

    commit - add env config

--- app/config.py
+++ app/config.py
@@ -1,1 +1,4 @@
-backend_url = 'https://kinhkcovid19dataengine.herokuapp.com/'
+import os
+backend_url = os.environ.get('backend_url')
+if backend_url is None:
+    backend_url = 'https://kinhkcovid19dataengine.herokuapp.com/'
